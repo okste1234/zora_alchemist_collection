13d0eaa33147d54a0a4703ab40fb913dc742ec64
okste1234 <okste1234@gmail.com>
2024-02-19 16:22:53 +0100

    CHORE: add new page as sample for goldrushkit testing

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,20 @@
-require("dotenv").config();
+// "use client"
+// // require("dotenv").config();
+// import "@covalenthq/goldrush-kit/styles.css";
 
-import { GoldRushProvider } from "@covalenthq/goldrush-kit"
+
+// import { GoldRushProvider } from "@covalenthq/goldrush-kit"
+import GoldRushExample from "./Sample";
 
 
 export default function Home() {
   return (
-    <GoldRushProvider apikey={process.env.APP_API_KEY}>
-
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-      </main>
-    </GoldRushProvider>
+    <main className="">
+      <GoldRushExample />
+      {/* <GoldRushProvider apikey={process.env.APP_API_KEY}> */}
+      {/* <h3>Hello, oo</h3> */}
+      {/* </GoldRushProvider> */}
+    </main>
   );
 }
 
