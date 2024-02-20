c5e290cb71772167418ebc392eec1d10668b5a4a
okste1234 <okste1234@gmail.com>
2024-02-20 14:25:01 +0100

    FIX: use the right goldrush kit component to display NFT collections

--- src/app/Sample.jsx
+++ src/app/Sample.jsx
@@ -2,10 +2,8 @@
 import "@covalenthq/goldrush-kit/styles.css";
 import {
     GoldRushProvider,
-    NFTWalletTokenListView,
-    TokenBalancesListView,
-    TokenTransfersListView,
-    AddressActivityListView,
+    NFTCollectionTokenListView,
+
 } from "@covalenthq/goldrush-kit";
 
 export default function GoldRushExample() {
@@ -16,32 +14,9 @@ export default function GoldRushExample() {
                 mode="dark"
                 color="emerald"
             >
-                <TokenBalancesListView
-                    chain_names={[
-                        "eth-mainnet",
-                        "matic-mainnet",
-                        "bsc-mainnet",
-                        "avalanche-mainnet",
-                        "optimism-mainnet",
-                    ]}
-                    hide_small_balances
-                    address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
-                />
-                <TokenTransfersListView
-                    chain_name="eth-mainnet"
-                    address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
-                    contract_address="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
-                />
-                <AddressActivityListView address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de" />
-                <NFTWalletTokenListView
-                    address="0xfc43f5f9dd45258b3aff31bdbe6561d97e8b71de"
-                    chain_names={[
-                        "eth-mainnet",
-                        "matic-mainnet",
-                        "bsc-mainnet",
-                        "avalanche-mainnet",
-                        "optimism-mainnet",
-                    ]}
+                <NFTCollectionTokenListView
+                    collection_address="0xfeee3700698f8d75bcc18e009022c7b44d2af44f"
+                    chain_name={"zora-mainnet"}
                 />
             </GoldRushProvider>
         </main>
